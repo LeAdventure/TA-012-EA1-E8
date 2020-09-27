d3aef7cd378df874b7b59820741d7b584f258069
Sebastian Terrazas <sebastian.terrazas@hotmail.com>
2020-09-26 23:03:58 -0500

    Actualizado

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,27 +4,20 @@
 #include <locale>
 
 using namespace std;
-
-//		[0-9]+[^a-rt-zA-RT-Z0-9][^a-su-zA-RT-Z0-9][^a-rt-zA-RT-Z0-9][.][^a-zA-Z02-9][^a-zA-Z0-79][^a-zA-Z0-35-9][^a-zA-Z0-689][^a-zA-Z0-24-9][^a-zA-Z02-9][^a-zA-Z0-689]
-// 		[0-9]+[^a-rt-zA-RT-Z\d][^a-su-zA-RT-Z\d][^a-rt-zA-RT-Z\d][.][^a-zA-Z02-9][^a-zA-Z0-79][^a-zA-Z0-35-9][^a-zA-Z0-689][^a-zA-Z0-24-9][^a-zA-Z02-9][^a-zA-Z0-689]
-
-// 		[0-9][13478A-Za-z.]*[.]1847317
-//      [0-9][13478A-Za-z.]*\.1847317
-
-
+//      Tests
 //      3sts.terrazas1sts.1.1847317 
 //      1sts.1847317
-//      0sts.1847317
 
 int main() {
     setlocale(LC_ALL, "");
     string str;
     int p = 0;
-    cout << "Nombre usado: Sebastian Terrazas Santillana \tMatricula usada: 1847317" << endl << endl;
+    cout << "Nombre usado: Sebastian Terrazas Santillana \tMatricula usada: 1847317" << endl;
+    cout << "Alfabeto = { 1, 3, 4, 7, 8, a, b, e, i, l, n, s, t, r, z, '.' }"; 
     cout << "Ingrese una cadena de caracteres: ";
     fflush(stdin);
     getline(cin, str);
-    regex e("[13478][13478A-Za-z\.]*\.1847317");
+    regex e("[13478][13478abeilnstrz\.]*\.1847317");
     regex f("sts.", regex_constants::icase);
 
     bool match1 = regex_match(str, e);
