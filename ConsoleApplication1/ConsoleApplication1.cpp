d6c80beb1bc8e9e7f74f5b576b81a809a25a0013
Sebastian Terrazas <sebastian.terrazas@hotmail.com>
2020-09-29 21:52:59 -0500

    Finished

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -17,11 +17,22 @@ int main() {
     setlocale(LC_ALL, "");
     string str, cont;
     int p = 0;
+    
+    cout << "Tarea - 1" << endl;
+    cout << "Miembros: " << endl;
+    cout << "José María Flores San Martín\t\t -\t1859565" << endl;
+    cout << "Miguel Eduardo Barragán Elizondo\t -\t1859404" << endl; 
+    cout << "Sebastián Terrazas Santillana\t\t -\t1847317" << endl;
+    getchar();
+    system("cls");
+    fflush(stdin);
 
     do{
         system("cls");
         cout << "Nombre: Sebastian Terrazas Santillana \tMatricula: 1847317" << endl;
         cout << "Alfabeto = { 1, 3, 4, 7, 8, a, b, e, i, l, n, s, t, r, z, '.' }" << endl << endl; 
+        cout << "Ejemplos de casos validos:\t" << "3sts.terrazas1sts.1.1847317\n\t\t\t\t3sts.terrazas1.1.1847317\n\t\t\t\t1sts.1847317\n\t\t\t\t3.terrstsazas1.1.1847317" << endl << endl;
+        cout << "Ejemplos de casos invalidos:\t" << "3.terrazas1.1.1847317\n\t\t\t\tsts.1847317\n\t\t\t\t1sts1847317\n\t\t\t\t1847317" << endl << endl;
         cout << "Ingrese una cadena de caracteres: ";
         fflush(stdin);
         getline(cin, str);
@@ -46,12 +57,12 @@ int main() {
             match = false;
         
         if (match == true)
-            cout << "Caso Valido" << endl;
+            cout << "Caso Valido" << endl << endl;
         else
-            cout << "Caso Invalido" << endl;
+            cout << "Caso Invalido" << endl << endl;
 
         do {
-            cout << "¿Desea continuar ingresando cadenas? [S/N]" << endl;
+            cout << "¿Desea introducir otra cadena? [S/N]" << endl << endl;
             fflush(stdin);
             getline(cin, cont);
             cont[0] = toupper(cont[0]);
@@ -62,7 +73,7 @@ int main() {
     }while (cont == "S");
 
 
-    cout << "Gracias por probar el programa" << endl;
+    cout << "Gracias por usar el programa" << endl;
     getchar();
 
 }
